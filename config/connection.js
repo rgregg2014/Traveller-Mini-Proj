24414f8133597a6140fe6a8ee597714925c8dbcf
Rachel Gregg <rgregg2014@gmail.com>
2021-08-20 14:04:17 -0400

    JAWS also makes me sad

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-let sequelize = new Sequelize();
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
